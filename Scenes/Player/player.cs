7ef2f53bd3c2dd9126e0c3d1ecee2a3f1141d380
PoisonFroggo <108835827+PoisonFroggo@users.noreply.github.com>
2024-01-08 10:37:30 -0800

    Player Controller update
    Updated the player controller. The player can now move around freely using mouse look. The body moves with the side to side movement of the mouse, and the camera moves with the up and down movement of the mouse. Vestigial attempts to make the body lag behind remain, but are inoperable. No animations have been added

--- Scenes/Player/player.cs
+++ Scenes/Player/player.cs
@@ -11,14 +11,18 @@ public partial class player : CharacterBody3D
 
 	[Export] public Node3D CameraNode { get; set; }
 	[Export] public Node3D PlayerModel { get; set; }
+	[Export] public Node3D PlayerRoot { get; set; }
 
 	
 	[Export] public float RotationSpeed { get; set; }
 	[Export] public float CameraActualRotationSpeed { get; set; }
+	[Export] public float BodyActualRotationSpeed { get; set; }
+	[Export] public float RootActualRotationSpeed { get; set; }
 	[Export] public float VerticalRotationLimit { get; set; } = 90; //we want the player to be able to spin when in midair, so this may prove unnecessary in the future
 
 	private Vector3 camTargetRotation;
-	private Vector3 bodyTargetRotation;
+	//private Vector3 bodyTargetRotation;
+	private Vector3 rootTargetRotation;
 
 
 	private float _rotationX = 0f;
@@ -48,7 +52,7 @@ public partial class player : CharacterBody3D
 
 		// Get the input direction and handle the movement/deceleration.
 		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_backward");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		Vector3 direction = Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y);
 		if (direction != Vector3.Zero)
 		{
 			velocity.X = direction.X * Speed;
@@ -62,6 +66,19 @@ public partial class player : CharacterBody3D
 
 		Velocity = velocity;
 		MoveAndSlide();
+
+		//handle camera up/down (x) rotation
+		CameraNode.Rotation = new Vector3(
+			Mathf.LerpAngle(CameraNode.Rotation.X, Mathf.DegToRad(camTargetRotation.X), CameraActualRotationSpeed * (float)delta),
+			0,
+			0
+			);
+		//handle player side to side (y) rotation
+		PlayerRoot.Rotation = new Vector3(
+				0,
+				Mathf.LerpAngle(PlayerRoot.Rotation.Y, Mathf.DegToRad(rootTargetRotation.Y), RootActualRotationSpeed * (float)delta),
+				0
+			);
 	}
 		
 	public override void _Input(InputEvent @event)
@@ -69,16 +86,21 @@ public partial class player : CharacterBody3D
 		//detects and responds to user mouse movement
 		if (@event is InputEventMouseMotion mouseMotion)
 		{
-			//Debug.WriteLine("mouse input detected");
-			camTargetRotation = new Vector3(
-				Mathf.Clamp((-1 * mouseMotion.Relative.Y * RotationSpeed) + camTargetRotation.X, -VerticalRotationLimit, VerticalRotationLimit),
-				//0,
-				Mathf.Wrap((-1 * mouseMotion.Relative.X * RotationSpeed) + camTargetRotation.Y, 0, 360), 
-				0);
-				Debug.Print(camTargetRotation.ToString());
-			CameraNode.Rotation = camTargetRotation;
 			//rotate the characterbody node on the Y axis when mouse is moved side to side
 			//rotate characterbody node on the X axis when mouse is moved up and down (later change this to the hip bone on the player skeleton)
+			//Debug.WriteLine("mouse input detected");
+			//calculate camera x rotation
+			camTargetRotation = new Vector3(
+				Mathf.Clamp((-1 * mouseMotion.Relative.Y * RotationSpeed) + camTargetRotation.X, -VerticalRotationLimit, VerticalRotationLimit),
+				0, 
+				0);
+			//calculate y rotation of the entire player
+			rootTargetRotation = new Vector3(
+				0,
+				Mathf.Wrap((-1 * mouseMotion.Relative.X * RotationSpeed) + rootTargetRotation.Y, 0, 360), 
+				0
+			);
+		
 		}
 
 		if (@event.IsActionPressed("escape")){
